61a067d1264a6efd42fe219ddf8780aaa6765c5e
Mateus Silva Seixas <mateusinhokarambi@hotmail.com>
2021-12-02 21:13:29 -0300

    Adicionando repetição ao jogo

--- jogo.c
+++ jogo.c
@@ -10,49 +10,51 @@ int main() {
   // Aqui declaramos as variáveis que vamos utilizar no jogo
   int numeroVerdadeiro, numerosRepetidos, linhaVerdadeiro, colunaVerdeiro, i, j, resultado;
 
-  // Alteramos a seed do rand() para gerar sempre números aleatórios baseados na data
-  srand(time(NULL));
-  
-  // Nós geramos os números aleatórios para cada uma das variáveis (entre 0 e 9)
-  linhaVerdadeiro = rand() % LINHAS;
-  colunaVerdeiro = rand() % COLUNAS;
-
-  // Sorteia o numerosRepetidos e numeroVerdeiro até que os números gerados sejam diferentes
   do {
-    numeroVerdadeiro = rand() % 10;
-    numerosRepetidos = rand() % 10;
-  } while(numeroVerdadeiro == numerosRepetidos);
+    // Alteramos a seed do rand() para gerar sempre números aleatórios baseados na data
+    srand(time(NULL));
+    
+    // Nós geramos os números aleatórios para cada uma das variáveis (entre 0 e 9)
+    linhaVerdadeiro = rand() % LINHAS;
+    colunaVerdeiro = rand() % COLUNAS;
 
-  // Desenhamos o nosso campo com (#)
-  for(i=0;i<LINHAS;i++) {
-    for(j=0;j<COLUNAS;j++) {
+    // Sorteia o numerosRepetidos e numeroVerdeiro até que os números gerados sejam diferentes
+    do {
+      numeroVerdadeiro = rand() % 10;
+      numerosRepetidos = rand() % 10;
+    } while(numeroVerdadeiro == numerosRepetidos);
 
-      // Verifica se a linha atual e a coluna atual correspondem à linha e coluna geradas randomicamento
-      if(i == linhaVerdadeiro && j == colunaVerdeiro) {
-        printf ("%d ", numeroVerdadeiro); // Imprimir o número verdadeiro
-      } else {
-        printf("%d ", numerosRepetidos); // Imprimir os números repetidos
+    // Desenhamos o nosso campo com (#)
+    for(i=0;i<LINHAS;i++) {
+      for(j=0;j<COLUNAS;j++) {
+
+        // Verifica se a linha atual e a coluna atual correspondem à linha e coluna geradas randomicamento
+        if(i == linhaVerdadeiro && j == colunaVerdeiro) {
+          printf ("%d ", numeroVerdadeiro); // Imprimir o número verdadeiro
+        } else {
+          printf("%d ", numerosRepetidos); // Imprimir os números repetidos
+        }
       }
+      printf ("\n");
     }
-    printf ("\n");
-  }
 
-  // Esperar 3 segundos
-  system("sleep 3");
+    // Esperar 3 segundos
+    system("sleep 3");
 
-  // Limpa a tela
-  system("clear");
+    // Limpa a tela
+    system("clear");
 
-  // Pedir ao usuário o resultado do jogo
-  printf ("\nDigite o número que não foi repetido: ");
-  scanf ("%d", &resultado);
+    // Pedir ao usuário o resultado do jogo
+    printf ("\nDigite o número que não foi repetido: ");
+    scanf ("%d", &resultado);
 
-  // Verifica se o resultado que o usuário digitou é igual ao número verdadeiro gerado
-  if (resultado == numeroVerdadeiro) {
-    printf ("\nACERTOU!!");
-  } else {
-    printf ("\nERROU");
-  }
+    // Verifica se o resultado que o usuário digitou é igual ao número verdadeiro gerado
+    if (resultado == numeroVerdadeiro) {
+      printf ("\nACERTOU!!\n\n");
+    } else {
+      printf ("\nERROU\n\n");
+    }
+  } while(1);
 
   return 0;
 }
